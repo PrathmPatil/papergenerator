79e804bbfc41ee2f1bc91c10a5335fd3474221f3
PrathmPatil <prathmpatil2818@gmail.com>
2026-05-31 12:44:55 +0530

    added new api

--- python/main.py
+++ python/main.py
@@ -1,11 +1,14 @@
 from app import create_app
 import os
 
-
 app = create_app()
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PDF_SERVICE_PORT", "5001"))
     debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
-    app.run(host="127.0.0.1", port=port, debug=debug)
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=debug
+    )
